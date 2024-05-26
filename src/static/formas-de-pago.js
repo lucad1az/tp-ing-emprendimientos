8c12e0a913ca94ab5f67cac3cc362847551d7e19
Luca <luca-diaz@live.com>
2024-05-26 03:57:05 -0300

    agrego logo y carpeta static con array de datos

---
+++ src/static/formas-de-pago.js
@@ -0,0 +1,7 @@
+const formas_de_pago = [
+    { value: "efectivo", label: "Efectivo"},
+    { value: "transferencia", label: "Transferencia"},
+    { value: "credito", label: "Credito"},
+    { value: "debito", label: "Debito"}
+]
+export default formas_de_pago;
---
+++ src/static/rubros.js
@@ -0,0 +1,11 @@
+const rubros = [
+    { value: "reparacion", label: "Reparación"},
+    { value: "musica", label: "Música"},
+    { value: "belleza", label: "Belleza"},
+    { value: "tecnologia", label: "Tecnología"},
+    { value: "moda", label: "Moda"},
+    { value: "electrodomesticos", label: "Electrodomesticos"},
+    { value: "entretenimiento", label: "Entretenimiento"},
+    { value: "otros", label: "Otros"}
+]
+export default rubros;
